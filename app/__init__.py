84089b40b1f08e8311c160306ceba91cb6519ca5
Roman Peters <github@romanpeters.nl>
2021-06-18 14:25:56 +0200

    Prepare flask_login

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,14 @@
 import secrets
 from flask import Flask
 from flask_bootstrap import Bootstrap
+# from flask_login import LoginManager
 
 app = Flask(__name__)
 app.secret_key = secrets.token_urlsafe()
 bootstrap = Bootstrap(app)
+# login_manager = LoginManager()
+# login_manager.init_app(app)
+# login_manager.login_view = "index"
+# login_manager.login_message = "Please login first."
 
 from app import views, api, errors
