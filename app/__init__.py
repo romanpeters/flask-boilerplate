f8e0642f791a0dac80fc2fa7ee212defd8ef143d
Roman Peters <github@romanpeters.nl>
2021-06-18 14:18:30 +0200

    Add Bootstrap CSS

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
+import secrets
 from flask import Flask
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+app.secret_key = secrets.token_urlsafe()
+bootstrap = Bootstrap(app)
 
 from app import views, api
